57579508920cc74593a2607b5d392cb8b8119093
yerin512 <sosu0512>
2021-04-20 16:59:06 +0900

    props

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ function App() {
   }
   return (
     <div className="App">
-      <Hello />
-      <Hello />
-      <Hello />
-      
+      <Hello age={10} />
+      <Hello age={20} />
+      <Hello age={30} />
+
     </div>
   );
 }
--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,15 +1,22 @@
 import { useState } from "react"; 
+import UserName from "./UserName";
 
-
-export default function Hello(){
+export default function Hello({ age }){
     const [name, setName] = useState("Mike");
+    const msg = age > 19 ? "성인 입니다." : "미성년자 입니다.";
 
     return (
     <div>
-       <h2 id="name">{name}</h2>
-       <button onClick={()=>{
+       <h2>
+           {name}({age}) : {msg}
+        </h2>
+        <UserName name={name}/>
+       <button onClick={() => {
              setName(name === "Mike" ? "Jane" : "Mike")
-    }}>Change</button>
+    }}
+    >
+        Change
+    </button>
     </div> 
     );
 }
