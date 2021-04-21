740727b50af57d73ffb65691a0a76cdd9baa34c2
yerin512 <sosu0512@naver.com>
2021-04-21 16:45:47 +0900

    전날 다음날버튼 추가하기 실패...

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,13 +1,22 @@
 import Word from "../component/Word";
 import { useParams } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
+import { useHistory } from "react-router";
+
 
 export default function Day(){
     const { day } = useParams();
     const words = useFetch(`http://localhost:3001/words?day=${day}`);
+    const days = useFetch(`http://localhost:3001/day=${day-1}`);
+    const history = useHistory();
+   
+        const onClick = (days) => {
+            history.push(days)
+        }
+
  return(
     <>
-      <h2>Day {day}</h2>
+      <h2>Day {day}</h2><button onClick={onClick}>전날</button><button>다음날</button>
       <table>
           <tbody>
               {words.map(word => (
